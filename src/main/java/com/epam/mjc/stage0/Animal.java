ee9937e2ff167197682edbe2f8c3c3283511358d
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-11-27 11:17:08 +0500

    Finished OOP final task

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,17 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(String color, int numberOfPaws, boolean hasFur) {
+        return "This animal is mostly " + color + ". Is has " + numberOfPaws + (numberOfPaws == 1 ? "paw and " : " paws and ") + (hasFur ? "a" : "no") + " fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,9 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
+    Dog(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+    }
 
+    Dog dog = new Dog("brown", 4, true);
 }
